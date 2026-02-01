1f446e2151d028dfff83fc218c1d5696b2477a19
LordVennin <121144033+LordVennin@users.noreply.github.com>
2026-02-01 18:53:55 +0000

    Ensure stingers are consumable ammo

---
+++ Content/Items/StingerAmmoGlobalItem.cs
@@ -0,0 +1,18 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace VenninBeeMod.Content.Items
+{
+    public class StingerAmmoGlobalItem : GlobalItem
+    {
+        public override void SetDefaults(Item entity)
+        {
+            if (entity.type == ItemID.Stinger)
+            {
+                entity.ammo = ItemID.Stinger;
+                entity.consumable = true;
+            }
+        }
+    }
+}
